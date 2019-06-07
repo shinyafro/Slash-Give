12d5140d18a15c58c019b85238ccecce75a1026c
= <ShinyTourettesX@gmail.com>
2019-06-07 12:05:16 +0800

    Refactored and added meta data TODO: String -> JSON -> NBT on item

---
+++ src/main/java/net/dirtcraft/plugin/slashgive/SlashGive.java
@@ -0,0 +1,41 @@
+package net.dirtcraft.plugin.slashgive;
+
+import net.dirtcraft.plugin.slashgive.command.Give;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.args.GenericArguments;
+import org.spongepowered.api.command.spec.CommandSpec;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.Order;
+import org.spongepowered.api.event.game.state.GameConstructionEvent;
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.text.Text;
+
+@Plugin(
+        id = "slash-give",
+        name = "Slash Give",
+        description = "Control giving of items to players.",
+        url = "https://dirtcraft.net/",
+        authors = {
+                "juliann",
+                "ShinyAfro"
+        }
+)
+public class SlashGive {
+
+    @Listener (order = Order.FIRST)
+    public void onConstructionEvent(GameConstructionEvent event) {
+        CommandSpec give = CommandSpec.builder()
+                .permission("slashgive.exempt")
+                .arguments(
+                        GenericArguments.playerOrSource(Text.of("target")),
+                        GenericArguments.string(Text.of("item")),
+                        GenericArguments.optional(GenericArguments.integer(Text.of("quantity"))),
+                        GenericArguments.optional(GenericArguments.integer(Text.of("meta")))
+                        //GenericArguments.optional(GenericArguments.requiringPermission(GenericArguments.string(Text.of("nbt")),"slashgive.nbt"))
+                )
+                .executor(new Give())
+                .build();
+
+        Sponge.getCommandManager().register(this, give, "give", "i", "minecraft:give", "randomtweaks:give");
+    }
+}
---
+++ src/main/java/net/dirtcraft/plugin/slashgive/command/Give.java
@@ -0,0 +1,45 @@
+package net.dirtcraft.plugin.slashgive.command;
+
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.command.CommandException;
+import org.spongepowered.api.command.CommandPermissionException;
+import org.spongepowered.api.command.CommandResult;
+import org.spongepowered.api.command.CommandSource;
+import org.spongepowered.api.command.args.CommandContext;
+import org.spongepowered.api.command.spec.CommandExecutor;
+import org.spongepowered.api.data.DataQuery;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.item.ItemType;
+import org.spongepowered.api.item.inventory.ItemStack;
+import org.spongepowered.api.text.Text;
+
+public class Give implements CommandExecutor {
+    @Override
+    public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
+        CommandSource target = args.<CommandSource>getOne("target").get();
+        String itemName = args.<String>getOne("item").get();
+        Integer quantity = args.<Integer>getOne("quantity").orElse(1);
+        Integer meta = args.<Integer>getOne("meta").orElse(0);
+        //String nbtData = args.<String>getOne("nbt").orElse(null); TODO - Figure out how to parse a string into NBTTagCompound and add it to an item if the user has the permissions.
+
+        if (src != target && !src.hasPermission("slashgive.others")){
+            throw new CommandPermissionException(Text.of("You don't have permission to give items to other players!"));
+        }
+        if (!Sponge.getRegistry().getType(ItemType.class, itemName).isPresent()){
+            throw new CommandException(Text.of("Invalid item"));
+        }
+
+        ItemStack item = ItemStack.builder()
+                .itemType(Sponge.getRegistry().getType(ItemType.class, itemName).get())
+                .quantity(quantity)
+                .build();
+
+        //We rebuild the item to add the metadata
+        item = ItemStack.builder()
+                .fromContainer(item.toContainer().set(DataQuery.of("UnsafeDamage"),meta))
+                .build();
+
+        ((Player)target).getInventory().offer(item);
+        return CommandResult.success();
+    }
+}
